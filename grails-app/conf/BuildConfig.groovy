584e7ec4ab50b32c26f3560e3b7e13ee8374659c
Goran Ehrsson <goran@technipelago.se>
2013-11-30 15:45:36 +0100

    Upgraded codenarc to 0.19 and code-coverage to 1.2.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,7 +13,6 @@ grails.project.dependency.resolution = {
     }
     dependencies {
     }
-
     plugins {
         build(":tomcat:$grailsVersion",
                 ":hibernate:$grailsVersion",
@@ -21,7 +20,40 @@ grails.project.dependency.resolution = {
                 ":rest-client-builder:1.0.3") {
             export = false
         }
+        test(":codenarc:0.19") { export = false }
+        test(":code-coverage:1.2.7") { export = false }
+
         compile "grails.crm:crm-core:latest.integration"
         runtime "grails.crm:crm-security:latest.integration"
     }
 }
+
+codenarc {
+    reports = {
+        CrmXmlReport('xml') {
+            outputFile = 'target/CodeNarcReport.xml'
+            title = 'GR8 CRM CodeNarc Report'
+        }
+        CrmHtmlReport('html') {
+            outputFile = 'target/CodeNarcReport.html'
+            title = 'GR8 CRM CodeNarc Report'
+
+        }
+    }
+    properties = {
+        GrailsPublicControllerMethod.enabled = false
+        CatchException.enabled = false
+        CatchThrowable.enabled = false
+        ThrowException.enabled = false
+        ThrowRuntimeException.enabled = false
+        GrailsStatelessService.enabled = false
+        GrailsStatelessService.ignoreFieldNames = "dataSource,scope,sessionFactory,transactional,*Service,messageSource,grailsApplication,applicationContext,expose"
+    }
+    processTestUnit = false
+    processTestIntegration = false
+}
+
+coverage {
+    exclusions = ['**/radar/**']
+}
+
