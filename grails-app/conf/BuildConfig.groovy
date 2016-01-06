bcf7997d847e10872da900b4c5cf2671a055332f
Goran Ehrsson <goran@technipelago.se>
2016-01-06 20:40:47 +0100

    Added codenarc XML config for Jenkins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -36,11 +36,20 @@ grails.project.dependency.resolution = {
             export = false
         }
 
-        test(":codenarc:0.22") { export = false }
+        test(":codenarc:0.24.1") { export = false }
         test(":code-coverage:2.0.3-3") { export = false }
 
         compile ":crm-core:2.4.2"
         compile ":crm-security:2.4.2"
         compile ":decorator:1.1"
+    }
+}
+
+codenarc.reports = {
+    xmlReport('xml') {
+        outputFile = 'target/CodeNarcReport.xml'
+    }
+    htmlReport('html') {
+        outputFile = 'target/CodeNarcReport.html'
     }
 }
