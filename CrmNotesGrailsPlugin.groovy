7c671af99f08846a255eb91f592103dca9396ca6
Goran Ehrsson <goran@technipelago.se>
2016-04-04 14:15:34 +0200

    Next snapshot version

--- CrmNotesGrailsPlugin.groovy
+++ CrmNotesGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import org.codehaus.groovy.grails.commons.GrailsClassUtils
 
 class CrmNotesGrailsPlugin {
     def groupId = ""
-    def version = "2.4.2"
+    def version = "2.4.3-SNAPSHOT"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore']
